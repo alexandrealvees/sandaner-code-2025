f0bc50c8bb616d7ef10b1613180708c691e77626
Alexandre <alexandrealveees@outlook.com>
2025-07-21 13:01:44 -0300

    tudo ok agora

--- local/start-mysql.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Iniciando container MySQL no Docker...
-docker run -d ^
-  --name mysql-dev ^
-  -e MYSQL_ROOT_PASSWORD=senha123 ^
-  -e MYSQL_DATABASE=meubanco ^
-  -p 3306:3306 ^
-  mysql:8.0
